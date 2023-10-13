16cc17c4bf58cbe46df341c2b1275de7cbfd4012
Christoph Bornhardt <cbornhardt@outlook.com>
2023-10-13 02:39:09 -0300

    Create AideBool.cs

---
+++ Aide/AideBool.cs
@@ -0,0 +1,27 @@
+﻿namespace Aide;
+
+public static class AideBool
+{
+    public static bool AndCheck(params bool[] args)
+    {
+        foreach(bool b in args)
+            if(!b) 
+                return false;
+        return true;
+    }
+
+    public static bool OneValueOf<T>(T refValue, params T[] compare)
+    {
+        if(compare.Contains(refValue))
+            return true;
+        return false;
+    }
+
+    public static bool OrCheck(params bool[] args)
+    {
+        foreach(bool b in args)
+            if(b) 
+                return true;
+        return false;
+    }
+}
