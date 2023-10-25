d0207fe6171e93731ed2b059cceee9b7850b374e
Christoph Bornhardt <cbornhardt@outlook.com>
2023-10-25 01:51:56 -0300

    Update AideString.cs

--- Aide/AideString.cs
+++ Aide/AideString.cs
@@ -4,12 +4,12 @@ namespace Aide
 {
     public static class AideString
     {
-        public static string CamelToNormal(this string str, bool keepUpper = true)
+        public static string PascalToNormal(this string str)
         {
             string handleChars(char c, int i)
             {
                 if (i == 0)
-                    return c.ToString();
+                    return c.ToString().ToUpper();
                 if (char.IsUpper(c))
                     return $" {c}";
                 return c.ToString();
@@ -20,10 +20,42 @@ namespace Aide
             for (int i = 0; i < len; i++)
                 builder.Append(handleChars(str[i], i));
 
-            if (keepUpper)
-                return builder.ToString();
+            return builder.ToString();
+        }
 
-            return builder.ToString().ToLower();
+        public static string NormalToCamel(this string str)
+        {
+            string handleChars(char c, int i, int e)
+            {
+                if (i == 0)
+                    return c.ToString().ToLower();
+                if (e == 0)
+                    return c.ToString().ToUpper();
+                return c.ToString();
+            }
+
+            StringBuilder builder = new();
+            string[] parts = str.Split(" ");
+            int partsLen = parts.Length;
+
+            for(int i = 0; i < partsLen; i++)
+            {
+                int len = parts[i].Length;
+                for(int e = 0; e < len; e++)
+                    builder.Append(handleChars(parts[i][e], i, e));
+            }
+
+            return builder.ToString();
+        }
+
+        public static string RemoveSpecialCharacters(this string str)
+        {
+            string special = "\\|@#~€¬[]{}ª!\"·$%&/()=?¿^*¨ Ç_:;º'¡`+´ç-.,</>";
+
+            foreach (char c in special)
+                str = str.Replace(c.ToString(), "");
+
+            return str;
         }
     }
 }
