b1b68bdfa37e5d1e19a82b2d0b6113c9aee9821b
Christoph Bornhardt <cbornhardt@outlook.com>
2023-10-25 03:20:49 -0300

    Update AideString.cs

--- Aide/AideString.cs
+++ Aide/AideString.cs
@@ -48,6 +48,22 @@ namespace Aide
             return builder.ToString();
         }
 
+        public static string PascalToCamel(this string str)
+        {
+            string handleChars(char c, int i)
+            {
+                if(i == 0)
+                    return c.ToString().ToLower();
+                return c.ToString();
+            }
+
+            StringBuilder builder = new();
+            int len = str.Length;
+            for(int i = 0; i < len; i++)
+                builder.Append(handleChars(str[i], i));
+            return builder.ToString();
+        }
+
         public static string RemoveSpecialCharacters(this string str)
         {
             string special = "\\|@#~€¬[]{}ª!\"·$%&/()=?¿^*¨ Ç_:;º'¡`+´ç-.,</>";
