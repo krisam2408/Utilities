e06a58f1dbe16ab843f1e16c4ee800d845693632
Christoph Bornhardt <cbornhardt@outlook.com>
2024-05-06 15:00:43 -0400

    miaschatz

---
+++ MaiSchatz/MaiSchatzSettings.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace MaiSchatz;
+
+public sealed class MaiSchatzSettings
+{
+    public string Key { get; set; }
+    public string Endpoint { get; set; }
+    public bool IsEnabled { get; set; }
+
+    public MaiSchatzSettings() { }
+
+    public MaiSchatzSettings(IConfigurationSection configuration)
+    {
+        string? key = configuration.GetSection("Key").Value;
+        string? endpoint = configuration.GetSection("Endpoint").Value;
+        string? enabled = configuration.GetSection("Enabled").Value;
+
+        if (string.IsNullOrWhiteSpace(key))
+            throw new NullReferenceException(nameof(key));
+
+        if(string.IsNullOrWhiteSpace(endpoint))
+            throw new NullReferenceException(nameof(endpoint));
+
+        Key = key;
+        Endpoint = endpoint;
+
+        if (string.IsNullOrWhiteSpace(enabled))
+        {
+            IsEnabled = false;
+            return;
+        }
+
+        bool handleEnabled()
+        {
+            if (enabled == "true")
+                return true;
+            return false;
+        }
+
+        IsEnabled = handleEnabled();
+    }
+}
---
+++ MaiSchatz/MeinMai.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System.Text;
+
+namespace MaiSchatz;
+
+public sealed class MeinMai
+{
+    private readonly string m_endpointCall;
+    private readonly bool m_enabled;
+
+    public MeinMai(MaiSchatzSettings settings)
+    {
+        m_enabled = settings.IsEnabled;
+
+        m_endpointCall = settings
+            .Endpoint
+            .Replace("{API_KEY}", settings.Key);
+    }
+
+    public async Task<T?> CallAsync<T>() where T : class
+    {
+        if (!m_enabled)
+            return null;
+
+        using(HttpClient client = new())
+        using(MemoryStream ms = new())
+        {
+            HttpResponseMessage response = await client.GetAsync(m_endpointCall);
+            await response.Content.CopyToAsync(ms);
+            byte[] dataBuffer = ms.ToArray();
+            string data = Encoding.UTF8.GetString(dataBuffer);
+            T? result = JsonConvert.DeserializeObject<T>(data);
+
+            if(result == null)
+                throw new FormatException(data);
+
+            return result;
+        }
+    }
+}
