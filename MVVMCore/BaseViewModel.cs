add8149b0ee467a5967ccb8cf721f62d80836b0c
Christoph Bornhardt <cbornhardt@outlook.com>
2024-06-11 19:53:03 -0400

    mvvm

---
+++ MVVMCore/BaseViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MVVMCore;
+
+public abstract class BaseViewModel : ObservableProperty
+{
+    private bool m_enabled;
+    public bool Enabled { get { return m_enabled; } set { SetValue(ref m_enabled, value); } }
+
+    protected BaseViewModel() { }
+}
